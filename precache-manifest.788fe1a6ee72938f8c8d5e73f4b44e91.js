207f7b68029b7fd0bcbe80be48a475295cf3b1f9
Mariya_Zaya <mariyazayaschnikova@gmail.com>
2023-03-06 17:58:30 +0300

    Updates

---
+++ precache-manifest.788fe1a6ee72938f8c8d5e73f4b44e91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "144371b23910146a632e",
+    "url": "/site_GameOfThrones/static/css/main.5df02072.chunk.css"
+  },
+  {
+    "revision": "144371b23910146a632e",
+    "url": "/site_GameOfThrones/static/js/main.144371b2.chunk.js"
+  },
+  {
+    "revision": "894bac7f866fc0eda511",
+    "url": "/site_GameOfThrones/static/css/1.d99a3e42.chunk.css"
+  },
+  {
+    "revision": "894bac7f866fc0eda511",
+    "url": "/site_GameOfThrones/static/js/1.894bac7f.chunk.js"
+  },
+  {
+    "revision": "c66f86fba0cec4356822",
+    "url": "/site_GameOfThrones/static/js/runtime~main.c66f86fb.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/site_GameOfThrones/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "947ee8c454816df159fac505e926dc90",
+    "url": "/site_GameOfThrones/static/media/watch-game-of-thrones-tile.947ee8c4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/site_GameOfThrones/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "697b06728cf0d1a0a6a14ec2337dd2fe",
+    "url": "/site_GameOfThrones/index.html"
+  }
+];
